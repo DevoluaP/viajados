aa320a36514e8ff8cd0362a3329e1ed73455f6b4
Paulo Henrique <paulohenrique456moreira@gmail.com>
2025-04-15 19:30:40 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# Viajados
+
+Bem-vindo ao **Viajados**, seu companheiro de viagem definitivo! Desenvolvido com **React Native** no frontend e **Node.js** no backend, o Viajados torna o planejamento de suas aventuras simples e intuitivo. Explore hotéis e voos disponíveis, reserve sua hospedagem ou passagem com poucos cliques, favorite opções para revisitar depois e personalize seu perfil alterando foto e nome. Nosso objetivo é conectar viajantes a destinos incríveis, oferecendo uma experiência fluida, roteiros sob medida e dicas locais para tornar cada jornada inesquecível.
+
+## Tecnologias
+- **Frontend**: React Native  
+- **Backend**: Node.js  
+
+
+## Funcionalidades
+- Reserva de hoteis
+- Reserva de Voos
+- Favoritar voos
+- Favoritar Hoteis
+- Adicionar foto ao perfil
+- Mudar nome do usuario
+
+
+## Como Instalar
+Instalar todas dependências
+```sh
+npm install
+````
+
+## Pré-requisitos
+- Node.js  
+- npm ou yarn  
+- Expo
+
+## Regras
+- Todo Commit feito no projeto será analisado pelo Tech Lead (Julio Akamine);
+- Não será aceito commit's fora da padronização do código
+
+## 👥 Contribuidores
+
+[![Contributors](https://contrib.rocks/image?repo=JulioAkaminee/viajados)](https://github.com/JulioAkaminee/viajados/graphs/contributors)
+
+
+
+
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { View } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+function TabBarIcon({
+  name,
+  color,
+  focused,
+}: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+  focused: boolean;
+}) {
+  return (
+    <View style={{ alignItems: 'center' }}>
+      {focused && (
+        <View
+          style={{
+            width: 50,
+            height: 3,
+            backgroundColor: '#d81b60', 
+            borderRadius: 2,
+            marginBottom: 5,
+          }}
+        />
+      )}
+      <FontAwesome size={24} name={name} color={color} />
+    </View>
+  );
+}
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#ffe4e6',
+          borderTopWidth: 0,
+          height: 70,
+        },
+        tabBarIconStyle: { marginBottom: 5 },
+        tabBarLabelStyle: {
+          fontSize: 10,
+          fontWeight: 'bold',
+          textTransform: 'none',
+        },
+        tabBarActiveTintColor: '#d81b60',
+        tabBarInactiveTintColor: '#d81b60',
+      }}
+    >
+      <Tabs.Screen
+        name="explorar"
+        options={{
+          title: 'Explorar',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name="compass" color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="minhasViagens"
+        options={{
+          title: 'Minhas Viagens',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name="plane" color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favoritos"
+        options={{
+          title: 'Favoritos',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name="heart" color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="minhaConta"
+        options={{
+          title: 'Minha Conta',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name="user" color={color} focused={focused} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
